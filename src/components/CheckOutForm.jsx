ad1076077cad5f5c0face725f602997e27d39b52
GOD <rameshrajendranb9919@gmail.com>
2023-12-17 17:27:13 +0530

    validation

--- src/components/CheckOutForm.jsx
+++ src/components/CheckOutForm.jsx
@@ -16,27 +16,27 @@ const CheckOutForm = ({ total, cartList }) => {
         <h3 className="text-center mb-3">Customer information</h3>
         <div className="flex flex-col md:grid md:grid-cols-2">
           <input
-            className="border border-black w-72 m-1 rounded-md outline-none p-1"
+            className="border border-black w-72 m-1 rounded-md outline-none p-1 focus:border-blue-300"
             type="text"
             placeholder="Enter your First name"
             required
           />
           <input
-            className="border border-black w-72 m-1 rounded-md outline-none p-1"
+            className="border border-black w-72 m-1 rounded-md outline-none p-1 focus:border-blue-300"
             type="text"
             placeholder="Enter your Last name"
             required
           />
           <input
             type="email"
-            className="border border-black m-1 w-72 rounded-md outline-none p-1"
+            className="border border-black m-1 w-72 rounded-md outline-none p-1 focus:border-blue-300"
             placeholder="Enter your Email"
             required
           />
           <input
-            type="text"
+            type="tel"
             placeholder="Enter mobile Number"
-            className="border border-black w-72 m-1 rounded-md outline-none p-1"
+            className="ap border border-black w-72 m-1 rounded-md outline-none p-1 focus:border-blue-300"
             required
           />
         </div>
@@ -47,15 +47,15 @@ const CheckOutForm = ({ total, cartList }) => {
 
         <div className="flex flex-col md:flex-row">
           <input
-            className="border border-black m-1 w-72 rounded-md outline-none p-1"
+            className="border border-black m-1 w-72 rounded-md outline-none p-1 focus:border-blue-300"
             type="text"
             placeholder="Your address"
             required
           />
           <input
             type="text"
-            className="border border-black m-1 w-72 rounded-md outline-none p-1"
-            placeholder="landmark"
+            className="border border-black m-1 w-72 rounded-md outline-none p-1 focus:border-blue-300"
+            placeholder="Landmark"
           />
         </div>
       </div>
@@ -64,15 +64,10 @@ const CheckOutForm = ({ total, cartList }) => {
         <h3 className="text-center mb-3">Payment method</h3>
         <div className="flex flex-col md:flex-row items-center">
           <input
-            type="password"
-            className="border border-black m-1 w-72 rounded-md outline-none p-1"
-            placeholder="Card number"
-          />
-          <p>or</p>
-          <input
             type="text"
-            className="border border-black m-1 w-72 rounded-md outline-none p-1"
-            placeholder="UPI ID"
+            className="border border-black m-1 w-72 rounded-md outline-none p-1 focus:border-blue-300"
+            placeholder="UPI"
+            required
           />
         </div>
       </div>
